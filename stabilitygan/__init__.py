a8138316d306f73c81e3b046f2b13b1d24cfd0bd
Ashmita Chakraborty <aschakra_ashim@outlook.com>
2026-08-31 13:23:09 +0000

    Add package initialization and version

---
+++ stabilitygan/__init__.py
@@ -0,0 +1,7 @@
+"""StabilityGAN: differentiable howling suppression trained against a
+learned maximum-stable-gain surrogate.
+
+Extends faust-icc (https://github.com/Dhwaani/In-CarCommunication).
+"""
+
+__version__ = "0.1.0"
